b95dafe2b3882ba78a8c0edec2d0a644dc753578
guofei <guofei1986@gmail.com>
2014-05-15 00:25:05 +0900

    add aritcle model

---
+++ spec/factories/articles.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :article do
+    title "title"
+    brief "a good article"
+    text "this is a good article.hello word nihao konitiwa"
+  end
+end
