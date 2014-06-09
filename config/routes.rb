4bd76f78fb00d30d6fd4188520c9dbd71ea384ed
guofei <guofei1986@gmail.com>
2014-06-10 01:50:07 +0900

    add rails_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_for :admin_users
+  mount RailsAdmin::Engine => '/admin_user', as: 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
