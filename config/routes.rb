93710fe3bc0cf18c9d2495422a3263c541b5abc4
guofei <guofei1986@gmail.com>
2014-06-12 00:15:36 +0900

    add user views

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   #   resources :products
   resources :articles
   resources :translations, only:[:create]
+  resources :users, only:[:show]
 
   # Example resource route with options:
   #   resources :products do
