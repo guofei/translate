40f3242dcbe418a75ca8bdae65b976b9b2db9163
guofei <guofei1986@gmail.com>
2014-11-10 16:17:54 +0900

    fix recaptcha

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,13 @@
 class CommentsController < ApplicationController
   def create
-    return redirect_to(commentable) unless verify_recaptcha
-    comment = Comment.new comment_params
-    commentable = params[:comment][:commentable_type].constantize.find(params[:comment][:commentable_id])
-    comment.commentable = commentable
-    comment.user = current_user
-    comment.save
+    if user_signed_in? || verify_recaptcha
+      comment = Comment.new comment_params
+      commentable = params[:comment][:commentable_type].constantize.find(params[:comment][:commentable_id])
+      comment.commentable = commentable
+      comment.user = current_user
+      comment.save
+    end
+
     redirect_to commentable
   end
 
