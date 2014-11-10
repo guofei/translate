b00060a1ff62444f2b4fa4014020d88bdc28dd8e
guofei <guofei1986@gmail.com>
2014-11-10 16:25:03 +0900

    fix comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
   def create
+    commentable = params[:comment][:commentable_type].constantize.find(params[:comment][:commentable_id])
     if user_signed_in? || verify_recaptcha
       comment = Comment.new comment_params
-      commentable = params[:comment][:commentable_type].constantize.find(params[:comment][:commentable_id])
       comment.commentable = commentable
       comment.user = current_user
       comment.save
